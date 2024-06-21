3aaa77234b7313ab56351d179010e4019d4c068e
trungtruongit <hieutrungtruong@gmail.com>
2024-06-21 21:28:42 +0700

    update relate product

--- src/pages-sections/sessions/Otp.jsx
+++ src/pages-sections/sessions/Otp.jsx
@@ -6,7 +6,7 @@ import BazaarImage from "components/BazaarImage";
 import axios from "axios";
 import { useRouter } from "next/router";
 import { MuiOtpInput } from "mui-one-time-password-input";
-import React from 'react'
+import React from "react";
 const fbStyle = {
     background: "#3B5998",
     color: "white",
@@ -46,7 +46,7 @@ const Otp = () => {
         setPasswordVisibility((visible) => !visible);
     }, []);
     const nav = useRouter();
-    const [otp, setOtp] = React.useState('')
+    const [otp, setOtp] = React.useState("");
     const handleOtp = (newValue) => {
         setOtp(newValue);
     };
