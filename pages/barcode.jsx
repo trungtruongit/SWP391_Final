a68b68bd6c0eb97faa5db01066774037fea4e3c8
trungtruongit <hieutrungtruong@gmail.com>
2024-06-23 15:35:06 +0700

    update barcode

--- pages/barcode.jsx
+++ pages/barcode.jsx
@@ -1,15 +1,41 @@
-import SEO from "components/SEO";
-import Login from "pages-sections/sessions/Login";
-import { FlexRowCenter } from "components/flex-box";
-import ShopLayout1 from "components/layouts/ShopLayout1";
-import { Box, useTheme } from "@mui/material";
-const BarCodePage = () => {
-    const theme = useTheme();
-    return;
-    <ShopLayout1 topbarBgColor={theme.palette.grey[900]}>
-        <SEO title="FourGemsShop" />
-        <Box bgcolor="#FFFFFF" />
-    </ShopLayout1>;
+import { useState, useEffect } from "react";
+import useScanDetection from "use-scan-detection";
+
+const BarCodeTest = () => {
+    const [barCode, setBarcodeScan] = useState("No Barcode Scanned");
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    // Ensure the hook is used only after the component has mounted
+    useEffect(() => {
+        if (isClient) {
+            const onComplete = (code) => {
+                setBarcodeScan(code);
+            };
+
+            const handler = (event) => {
+                const barcode = event.key;
+                if (barcode.length >= 6) {
+                    onComplete(barcode);
+                }
+            };
+
+            document.addEventListener("keydown", handler);
+
+            return () => {
+                document.removeEventListener("keydown", handler);
+            };
+        }
+    }, [isClient]);
+
+    return (
+        <div>
+            <h1>Barcode:{barCode}</h1>
+        </div>
+    );
 };
 
-export default BarCodePage;
+export default BarCodeTest;
